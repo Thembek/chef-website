aca3c12cad97d47acbc0502ff56febb4cf6b9e0f
Andile <andile.goba@younglings.africa>
2022-10-20 16:15:54 +0200

    about chef and findus update commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
+import { AboutUs, Chef } from './container';
 import  Navbar  from './components/navbar/Navbar';
 import './App.css';
+
 
 function App() {
   return (
     <div>
       <Navbar />
+      <AboutUs />
+      <Chef />
     </div>
   );
 }
---
+++ src/container/index.js
@@ -0,0 +1,21 @@
+import AboutUs from './aboutus/Aboutus';
+import Chef from './chef/Chef';
+import FindUs from './FindUs/FindUs';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import Laurels from './Laurels/Laurels';
+import SpecialMenu from './Menu/SpecialMenu';
+
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
