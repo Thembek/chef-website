63656eee722e0e78ee0e1446017899e31738e577
Andile <andile.goba@younglings.africa>
2022-10-20 10:09:03 +0200

    menuitem and footer update commit

---
+++ src/components/footer/FooterOverlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footeroverlay.css';
+
+const Footeroverlay = () => (
+    <div className="app__footeroverlay">
+        <div className="app__footeroverlay-black" />
+        <div className="app__footeroverlay-img app__bg" />
+    </div>
+);
+
+export default Footeroverlay;
