15883d42bfff2bfc612d1519b60e6e19ac9c56ce
Andile <andile.goba@younglings.africa>
2022-10-20 16:21:44 +0200

    components indexjs update commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Subheading from './subheading/Subheading';
+import Newletter from './footer/Newsletter';
+import FooterOverlay from './footer/FooterOverlay';
+import Menuitem from './menuitem/Menuitem';
+import Navbar from './navbar/Navbar';
+
+export {
+    Subheading,
+    Newletter,
+    FooterOverlay,
+    Menuitem,
+    Navbar,
+}
---
+++ src/container/footer/Footer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
+import { FooterOverlay, Newsletter } from '../../components';
+import './footer.css';
